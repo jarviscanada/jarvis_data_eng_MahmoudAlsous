2c619b84e938f9e466f44bec0dde9ab535f6cea1
Mahmoud Al Sous <mahmoudalsous@cmail.carleton.ca>
2022-02-16 19:20:24 +0000

    Working addition of host_info

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -6,18 +6,15 @@ db_name=$3
 psql_user=$4
 psql_password=$5
 
-#Check # of args
 if [ $# -ne 5 ]; then
     echo "Illegal number of parameters"
     exit 1
 fi
 
-#Save machine statistics in MB and current machine hostname to variables
 lscpu_out=$(lscpu)
 mem_out=$(cat /proc/meminfo)
 hostname=$(hostname -f)
 
-#Retrieve hardware specification variables
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture\:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out"  | egrep "^Model name\:" | awk '{for(i=3;i<=NF;i++) printf $i" "; print ""}' | xargs)
@@ -26,11 +23,10 @@ l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache\:" | awk '{split($3,a,"K"); pri
 total_mem=$(echo "$mem_out"  | egrep "^MemTotal\:" | awk '{print $2}' | xargs)
 timestamp=$(vmstat -t | awk '{print $18,$19}'| tail -n1 | xargs)
 
-#PSQL command: Inserts server usage data into host_usage table
-#Note: be careful with double and single quotes
+
 insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, timestamp) VALUES('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp')"
-#set up env var for pql cmd
+
 export PGPASSWORD=$psql_password
-#Insert data into a database
+
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
