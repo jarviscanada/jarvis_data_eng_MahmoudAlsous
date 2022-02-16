d5d234e6acda0fc0308dc491ac1db6e5d2ea9791
Mahmoud Al Sous <mahmoudalsous@cmail.carleton.ca>
2022-02-16 00:28:24 +0000

    host_info Iteration #1

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+#Check # of args
+if [ $# -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+#Save machine statistics in MB and current machine hostname to variables
+vmstat_mb=$(vmstat --unit M)
+lscpu_out=`lscpu`
+mem_out=$(cat /proc/meminfo)
+hostname=$(hostname -f)
+
+#Retrieve hardware specification variables
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture\:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model name\:" | awk '{for(i=3;i<=NF;i++) printf $i" "; print ""}' | xargs)
+cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz\:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache\:" | awk '{print $3}' | xargs)
+total_mem=$(echo "$mem_out"  | egrep "^MemTotal\:" | awk '{print $2}' | xargs)
+timestamp=$(vmstat -t | awk '{print $18,$19}'| tail -n1 | xargs)
+
+#xargs is a trick to trim leading and trailing white spaces
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1 | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1 | xargs)
+disk_io=$(vmstat -d | awk '{print $10}' | tail -n1 | xargs)
+disk_available=$(df -BM | egrep "^/dev/sda2" | awk '{print $4}' | xargs)
+
+#Subquery to find matching id in host_info table
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+
+#PSQL command: Inserts server usage data into host_usage table
+#Note: be careful with double and single quotes
+insert_stmt="INSERT INTO host_usage(timestamp, ...) VALUES('$timestamp', ..."
+
+#set up env var for pql cmd
+export PGPASSWORD=$psql_password
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
