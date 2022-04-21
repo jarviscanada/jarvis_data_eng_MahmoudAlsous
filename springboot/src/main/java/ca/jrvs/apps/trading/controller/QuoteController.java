527a1caa674ef8214968b173279f53597d49a209
Mahmoud Al Sous <mahmoudalsous@cmail.carleton.ca>
2022-04-21 21:57:00 +0000

    QuoteDaoIntTest, QuoteService and QuoteServiceIntTest

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/QuoteController.java
@@ -2,6 +2,7 @@ package ca.jrvs.apps.trading.controller;
 
 import ca.jrvs.apps.trading.model.domain.IexQuote;
 import ca.jrvs.apps.trading.service.QuoteService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -27,6 +28,18 @@ public class QuoteController {
     public IexQuote getQuote(@PathVariable String ticker) {
         try {
             return quoteService.findIexQuoteByTicker(ticker);
+        } catch (Exception e) {
+            throw ResponseExceptionUtil.getResponseStatusException(e);
+        }
+    }
+
+    //@ApiOperation(value = "Update quote table using iex data", notes = "Update all quotes in the quote table. Use IEX trading API as market data source.")
+    @PutMapping(path = "/iexMarketData")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public void updateMarketData() {
+        try {
+            quoteService.updateMarketData();
         } catch (Exception e) {
             throw ResponseExceptionUtil.getResponseStatusException(e);
         }
--- springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/QuoteService.java
@@ -3,11 +3,14 @@ package ca.jrvs.apps.trading.service;
 import ca.jrvs.apps.trading.dao.MarketDataDao;
 import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.model.domain.IexQuote;
+import ca.jrvs.apps.trading.model.domain.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional
 @Service
@@ -25,6 +28,67 @@ public class QuoteService {
     }
 
     /**
+     * Update quote table against IEX source
+     *  - get all quotes from the db
+     *  - foreach ticker get iexQuote
+     *  - convert iexQuote to quote entity
+     *  - persist quote to db
+     *
+     * @throws org.springframework.dao.DataAccessException if unable to retrieve data
+     * @throws IllegalArgumentException                    for invalid input
+     */
+    public void updateMarketData() {
+        List<Quote> quotes = (List<Quote>) quoteDao.findAll();
+        List<IexQuote> iexQuotes = quotes.stream().map(quote -> findIexQuoteByTicker(quote.getId()))
+                .collect(Collectors.toList());
+        List<Quote> updatedQuotes = iexQuotes.stream().map(quote -> buildQuoteFromIexQuote(quote))
+                .collect(Collectors.toList());
+        quoteDao.saveAll(updatedQuotes);
+    }
+
+    /**
+     * Helper method. Map an IexQuote to a Quote entity Note: `iexQuote.getLatestPrice() == null if
+     * the stock market is close. Make sure set a default value for number fields(s).
+     */
+    protected static Quote buildQuoteFromIexQuote(IexQuote iexQuote) {
+        Quote quote = new Quote();
+        quote.setTicker(iexQuote.getSymbol());
+        quote.setBidPrice(iexQuote.getIexBidPrice());
+        quote.setBidSize(iexQuote.getIexBidSize());
+        quote.setAskPrice(iexQuote.getIexAskPrice());
+        quote.setAskSize(iexQuote.getIexAskSize());
+        return quote;
+    }
+
+    /**
+     * Validate (against IEX) and save given tickers to quote table - Get iexQuote(s) - convert each
+     * iexQuote to Quote entity - persist the quote to db
+     *
+     * @param tickers a list of tickers/symbols
+     * @throws IllegalArgumentException if ticker is not found from IEX
+     */
+    public List<Quote> saveQuotes(List<String> tickers) {
+        List<IexQuote> iexQuotes = tickers.stream().map(ticker -> findIexQuoteByTicker(ticker))
+                .collect(Collectors.toList());
+        List<Quote> quotes = iexQuotes.stream().map(quote -> buildQuoteFromIexQuote(quote))
+                .collect(Collectors.toList());
+
+        quotes.stream().forEach(quote -> saveQuote(quote));
+        return quotes;
+    }
+
+    /**
+     * Helper Method
+     * @param ticker
+     * @return
+     */
+    public Quote saveQuote(String ticker) {
+        IexQuote iexQuote = findIexQuoteByTicker(ticker);
+        Quote quote = buildQuoteFromIexQuote(iexQuote);
+        return quoteDao.save(quote);
+    }
+
+    /**
      * Find an IexQuote
      *
      * @param ticker id
@@ -33,5 +97,23 @@ public class QuoteService {
      */
     public IexQuote findIexQuoteByTicker(String ticker) {
         return marketDataDao.findById(ticker).orElseThrow(() -> new IllegalArgumentException(ticker + "is invalid"));
+    }
+
+    /**
+     * Update a given quote to quote table without validation
+     *
+     * @param quote
+     */
+    public Quote saveQuote(Quote quote) {
+        return quoteDao.save(quote);
+    }
+
+    /**
+     * Find all quotes from the quote table
+     *
+     * @return a list of quotes
+     */
+    public List<Quote> findAllQuotes() {
+        return (List<Quote>) quoteDao.findAll();
     }
 }
